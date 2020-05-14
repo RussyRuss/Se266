e09ab1ccb654baf15df2aae132f6a02de519ec4b
RussyRuss <53628177+RussyRuss@users.noreply.github.com>
2020-05-14 15:55:03 -0400

    save

--- ClassWebsite/Week8/index.php
+++ ClassWebsite/Week8/index.php
@@ -1,16 +1,24 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP and My SQL </title>
-    </head>
-    <body>
-		<h1>PHP and My SQL with Russel Souffrant</h1>
-        
-       
-       <?php
-            echo "Welcome to Russel's; PHP Page";
-        ?>
-       <h2>About Me: My Name is Russel Souffrant And i Am an Aspiring Softeware engineering in the making< this page will display my PHP and My SQL asignments for the quarter<h2>
-    </body>
-</html>
+<?php include __DIR__ . '/../include/header.php'; ?>
+    
+<h1>PHP and MySQL -Russel Souffrant</h1>
+<p>
+Russel's PHP and MySQL page that will present to you my assignments through out the course.
+</p>
+<h2>Assignment Overview</h2>
+
+  <ul>
+    <li><a href="../week1/index.php">Week 1</a></li>
+    <li><a href="../week2/index.php">Week 2</a></li>
+    <li><a href="../week3/index.php">Week 3</a></li>
+    <li><a href="../week4/index.php">Week 4</a></li>
+    <li><a href="../week5/index.php">Week 5</a></li>
+    <li><a href="../week6/index.php">Week 6</a></li>
+    <li><a href="../week7/index.php">Week 7</a></li>
+    <li><a href="../week8/index.php">Week 8</a></li>
+    <li><a href="../week9/index.php">Week 9</a></li>
+    <li><a href="../week10/index.php">Week 10</a></li>
+  </ul>
+    
+
+    
+  <?php include __DIR__ . '/../include/footer.php'; ?>

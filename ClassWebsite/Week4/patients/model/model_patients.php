d7ffb637e386ec406322e790e9b5f73ceb1255c0
RussyRuss <53628177+RussyRuss@users.noreply.github.com>
2020-06-02 15:47:10 -0400

    deleted

--- ClassWebsite/Week4/patients/model/model_patients.php
+++ ClassWebsite/Week4/patients/model/model_patients.php
@@ -1,42 +1,67 @@
 <?php
-    var_dump ($_POST);
-    //$firstName = filter_input(INPUT_POST, 'firstName'); 
-    $firstName = "";
-    $error = "";
-    if (isset($_POST['btnSubmit'])) {
-        $firstName = filter_input(INPUT_POST, 'firstName'); 
-        if (isset($_POST['oldEnoughToDrive'])) {
-            
-        }
-        if ($firstName == "") {
-            $error .= "No first name provided";
-        }
-        //echo "Submitted form";
-    } else {
-       // echo "Not yet submitted";
+
+include (__DIR__ . '/db.php');
+
+
+function patients () {
+    global $db;
+    
+    $results = [];
+
+    $stmt = $db->prepare("SELECT id, firstName, lastName, birthDate, age, married FROM patients ORDER BY firstName"); 
+    
+    if ( $stmt->execute() && $stmt->rowCount() > 0 ) {
+         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+             
+     }
+     
+     return ($results);
+}
+
+
+function addpatient ($t, $d) {
+    global $db;
+    $results = "Not added";
+
+    $stmt = $db->prepare("INSERT INTO patients SET firstName = :patient, lastName = :lastName");
+
+    $binds = array(
+        ":team" => $t,
+        ":division" => $d
+    );
+    
+    
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = 'Data Added';
     }
     
+    return ($results);
+}
+
+function addpatient2 ($t, $d) {
+    global $db;
+    $results = "Not added";
+
+    $stmt = $db->prepare("INSERT INTO patients SET firstName = :patient, division = :division");
+   
+    $stmt->bindValue(':team', $t);
+    $stmt->bindValue(':division', $d);
+   
+    $stmt->execute();
+    
+    if ($stmt->rowCount() > 0) {
+        $results = 'Data Added';
+    }
+   
+    $stmt->closeCursor();
+   
+    return ($results);
+}
+
+
+// $result = addTeam2 ('Feyenoord', 'Eredivisie');
+// echo $result;
+
+
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form example</title>
-</head>
-<body>
-    <?php
-        echo "$error";
-    ?>
-    <br />
-    <form method="post" action="formtest1.php">
-    <label>First Name:</label><input type="text" value="<?= $firstName?>" name="firstName" /><br />
-        <label>Old enough to drive</label>
-        <input type="checkbox" name="oldEnoughToDrive" value="yes">
-        <br />
-        <input type="submit" value="Check this Form" name="btnSubmit" />
-    </form>
-
-</body>
-</html>

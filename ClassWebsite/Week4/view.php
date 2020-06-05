491af6a3c2b515bb6251f6f2a99f45923ee21988
RussyRuss <53628177+RussyRuss@users.noreply.github.com>
2020-06-05 16:19:06 -0400

    commit

--- ClassWebsite/Week4/view.php
+++ ClassWebsite/Week4/view.php
@@ -1,20 +1,4 @@
-
-
-<html lang="en">
-<head>
-  <title>View Patients</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <div class="container">
-        
-    <div class="col-sm-offset-2 col-sm-10">
-        <h1>Patients</h1>
-    
+ 
     <?php
         
         include __DIR__ . '/model/model_patients.php';
@@ -29,6 +13,21 @@
 
         
     ?>
+<html lang="en">
+<head>
+  <title>View Patients</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        
+    <div class="col-sm-offset-2 col-sm-10">
+        <h1>Patients</h1>
+   
   
     <table class="table table-striped">
             <thead>
